4a5090136f6bdb1ba39b788643dc799631503010
Virgil Cai <ryujicai@hotmail.com>
2013-01-13 23:34:04 +0800

    login controller updated.

--- _login.php
+++ _login.php
@@ -1,19 +1,23 @@
 <?php
+header("Expires: 0");
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+
 //initialize session
 session_start();
 include('class/User.php');
 
-//call function to verify username and password
-$result = User::login($_POST['username'], $_POST['password']);
-
 //true if user name does or password does not match
-$_session["loginError"] = False;
+$_session["loginError"] = false;
 //page to be redirected to
 $destination = "index.php";
 
-switch($result){
+//call function to verify username and password
+$_session["userType"] = User::login($_POST['username'], $_POST['password']);
+
+switch($_session["userType"]){
     case "":        
-        $_session["loginError"] = True;
+        $_session["loginError"] = true;
         break;
     case "teacher":
         $destination = "/RTSS/relief/index.php";
