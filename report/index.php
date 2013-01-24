17d8e451b8869991ab6b862e55b26faa8377d957
mehere <yjavaw@gmail.com>
2013-01-24 12:45:13 +0800

    report UI

--- report/index.php
+++ report/index.php
@@ -1,5 +1,6 @@
 <?php 
-    include_once '../head-frag.php'; include_once '../php-head.php';
+    include_once '../php-head.php';
+    include_once '../head-frag.php';
 ?>
 <title><?php echo PageConstant::SCH_NAME_ABBR . " " . PageConstant::PRODUCT_NAME; ?></title>
 <link href="/RTSS/css/main.css" rel="stylesheet" type="text/css" />
@@ -18,13 +19,13 @@
 <div id="container">  	
     <div id="content-wrapper">
     	<div id="content">
-            <div id="topbar">
-            	<div class="fltrt">XXX | <a href="/RTSS/">Log out</a></div>
-                <ul class="breadcrumb">
-                    <li><a href="/RTSS/report/">Report</a></li>
-                    <li>View</li>
-                </ul>
-            </div>
+            <?php 
+                $TOPBAR_LIST=array(
+                    array('tabname'=>'Report', 'url'=>"/RTSS/report/"), 
+                    array('tabname'=>'View', 'url'=>""), 
+                );
+                include '../topbar-frag.php';
+            ?>
             <div id="tabs">
             	<div class="gradient-top"></div>
                 <ul>
