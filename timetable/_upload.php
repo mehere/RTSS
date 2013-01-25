cb3be57394c7221acbeb08dcffc4fa84e255ba69
Wee <Wee@Wee-Edge>
2013-01-25 15:34:50 +0800

    Created a namematch.php which will be a placeholder for new codes.

--- timetable/_upload.php
+++ timetable/_upload.php
@@ -1,14 +1,36 @@
 <?php
 
-if ($_FILES["timetableFile"]["error"] > 0)
-  {
-  echo "Error: " . $_FILES["timetableFile"]["error"] . "<br>";
-  }
-else
-  {
-  echo "Upload: " . $_FILES["timetableFile"]["name"] . "<br>";
-  echo "Type: " . $_FILES["timetableFile"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["timetableFile"]["size"] / 1024) . " kB<br>";
-  echo "Stored in: " . $_FILES["timetableFile"]["tmp_name"];
-  }
+spl_autoload_register(
+        function ($class) {
+            include '../class/' . $class . '.php';
+        });
+
+$year = $_POST["year"];
+$semester = $_POST["sem"];
+echo '<br>year:' . $year;
+echo '<br>sem:' . $semester;
+
+if ($_FILES["timetableFile"]["error"] > 0) {
+    echo "Error: " . $_FILES["timetableFile"]["error"] . "<br>";
+} else {
+    $fileName = $_FILES["timetableFile"]["tmp_name"];
+    $analyzer = new TimetableAnalyzer();
+    try {
+        $analyzer->readCsv($fileName);
+
+
+        $arrTeachers = $analyzer->arrTeachers;
+        $arrTeachers = Teacher::getTeachersAccnameAndFullname($arrTeachers);
+        $analyzer->printTeachers();
+
+        $destination = "/RTSS/timetable/index.php";
+
+        $_SESSION["timetableAnalyzer"] = $analyzer;
+    } catch (Exception $e) {
+        echo "Error: Wrong file<br>Message:".$e->getMessage();
+        /// To-Do: Where to forward to if there is error?
+        //$destination = ""
+    }
+    header("Location: $destination");
+}
 ?>
