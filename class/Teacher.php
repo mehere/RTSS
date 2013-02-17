68fefc6490843f2f54675524502a72bcfbf3ec47
Jerry Xue <martainflash@gmail.com>
2013-02-17 17:35:06 +0800

    setExcludedList

--- class/Teacher.php
+++ class/Teacher.php
@@ -482,6 +482,8 @@ class Teacher {
         }
         
         $_SESSION['excluded'][$date] = implode(",", $list);
+        
+        return true;
     }
     
     public static function getExcludingList($date)
