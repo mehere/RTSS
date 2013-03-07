7bdcca7914fc39191adb5b56b661f2ace7514eee
Wee <Wee@Wee-Edge>
2013-03-07 15:05:40 +0800

    Background Runner

--- TestScript.php
+++ TestScript.php
@@ -1,5 +1,10 @@
 <?php
-sleep(10);
-error_log("Successful 2");
-echo ("Successful 2 ");
+//sleep(10);
+$options = getopt("p:h:");
+$part = $options["p"];
+$part2 = $options["h"];
+error_log("p = $part");
+error_log("h = $part2");
+//echo ("$part");
+echo "run";
 ?>
