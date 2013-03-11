10c0f22875d4fda4e6050075eeea1d48ba0f4a4f
Jerry Xue <martainflash@gmail.com>
2013-03-12 00:02:31 +0800

    fix bug

--- class/Teacher.php
+++ class/Teacher.php
@@ -271,7 +271,7 @@ class Teacher {
                 }
                 if(strcmp($type, "all_normal_except_aed")===0)
                 {
-                    $sql_query_normal = "select user_id, user_name, dept_name from student_details where user_position = 'Teacher' and dept_name != $db_type[1] order by user_name;";
+                    $sql_query_normal = "select user_id, user_name, dept_name from student_details where user_position = 'Teacher' and dept_name != '$db_type[1]' order by user_name;";
                 }
             }
 
